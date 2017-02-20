7c48b6d396bed14aad6c0bec7ebbae64a6e5fcc3
Pavlo Ishchenko <pavlo.ishchenko@gmail.com>
2017-02-20 16:59:26 +0200

    Add Lesson1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+
+            string b = Console.ReadLine();
+
+            Console.WriteLine("You said:");
+
+            Console.WriteLine(b);
+
+            int a = Convert.ToInt32(b);
+
+
+            Console.WriteLine("Say answer");
+
+            string c = Console.ReadLine();
+
+            int answer = Convert.ToInt32(c);
+
+            if (answer == 42)
+            {
+                Console.WriteLine("Right answer");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect answer");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
